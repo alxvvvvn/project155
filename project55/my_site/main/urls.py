4ec9f846f537de6cac6ee37d097ebff414e58edb
alxvvvvn <bukina.y@sch2009.net>
2025-02-14 00:08:56 +0300

    Add files via upload

---
+++ project55/my_site/main/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from . import views 
+
+urlpatterns = [
+    path('', views.index ),
+    path('page', views.page),
+    path('page1', views.page1),
+    path('page2', views.page2),
+    path('page3', views.page3),
+    path('page4', views.page4),
+    path('page5', views.page5),
+    path('page6', views.page6),
+    path('page7', views.page7),
+    path('page8', views.page8),
+    path('page9', views.page9),
+    path('page10', views.page10),
+    path('page11', views.page11),
+    path('page12', views.page12),
+    path('page13', views.page13),
+    path('page14', views.page14),
+    path('page15', views.page15),
+]
---
+++ project55/my_site/main/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "main/index.html")
+
+def page(request):
+    return render(request, "main/page.html")
+
+def page1(request):
+    return render(request, "main/page1.html")
+
+def page2(request):
+    return render(request, "main/page2.html")
+
+def page3(request):
+    return render(request, "main/page3.html")
+
+def page4(request):
+    return render(request, "main/page4.html")
+
+def page5(request):
+    return render(request, "main/page5.html")
+
+def page6(request):
+    return render(request, "main/page6.html")
+
+def page7(request):
+    return render(request, "main/page7.html")
+
+def page8(request):
+    return render(request, "main/page8.html")
+
+def page9(request):
+    return render(request, "main/page9.html")
+
+def page10(request):
+    return render(request, "main/page10.html")
+
+def page11(request):
+    return render(request, "main/page11.html")
+
+def page12(request):
+    return render(request, "main/page12.html")
+
+def page13(request):
+    return render(request, "main/page13.html")
+
+def page14(request):
+    return render(request, "main/page14.html")
+
+def page15(request):
+    return render(request, "main/page15.html")
